5c4d75029e0df4b4dcf3a27f13a3656d777ca466
Aquti <103832857+Aquti@users.noreply.github.com>
2022-04-16 15:01:11 +0200

    Create HBE.lua

---
+++ HBE.lua
@@ -0,0 +1,27 @@
+-- Made by TrashScripterF, GUI Lib by Wally
+
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/BadScripter/Scripts/master/WallyGUILibV1.lua", true))()
+local hb = library:CreateWindow({
+  text = "Hitbox"
+})
+
+local credits = library:CreateWindow({text='Credits'})
+credits:AddLabel("Scripter: TrashScripterF\nGUI Lib: Wally")
+
+
+hb:AddBox("Size", function(object, focus)
+	if focus then
+    	for i, v in pairs(workspace.AIHunter:GetChildren()) do
+        if v:FindFirstChild("HumanoidRootPart") and v:FindFirstChild("UpperTorso") then
+          v.HumanoidRootPart.Size = Vector3.new(tonumber(object.Text),tonumber(object.Text),tonumber(object.Text))
+          v.Character.UpperTorso.Size = Vector3.new(tonumber(object.Text),tonumber(object.Text),tonumber(object.Text))
+        end
+		end
+	end
+end)
+
+game.StarterGui:SetCore("SendNotification", {
+	Title = "GUI Loaded",
+	Text = "Optimized by Aquti",
+	Duration = 5,
+})
